f614b02584c0a525340f39b50a38963511a9d5d2
Edoardo Panfili <edoardopa@gmail.com>
2019-11-20 20:51:27 +0100

    inserita bozza per filtrare docenti liberi in un'ora

---
+++ src/main/java/it/edu/iisgubbio/sostituzioni/filtri/FiltroLibero.java
@@ -0,0 +1,16 @@
+package it.edu.iisgubbio.sostituzioni.filtri;
+
+import it.edu.iisgubbio.sostituzioni.oggetti.Ora;
+
+import java.util.ArrayList;
+
+import it.edu.iisgubbio.sostituzioni.oggetti.Docente;
+
+public class FiltroLibero {
+    
+    public static ArrayList<Docente> docentiLiberi(ArrayList<Docente> tutti, Ora ora){
+        // TODO
+        return null;
+    }
+
+}
---
+++ src/test/java/it/edu/iisgubbio/sostituzioni/filtri/TestFiltroLibero.java
@@ -0,0 +1,22 @@
+package it.edu.iisgubbio.sostituzioni.filtri;
+
+import java.util.ArrayList;
+
+import it.edu.iisgubbio.sostituzioni.Elenchi;
+import it.edu.iisgubbio.sostituzioni.oggetti.Docente;
+import it.edu.iisgubbio.sostituzioni.oggetti.Ora;
+
+public class TestFiltroLibero {
+
+    public static void main(String[] args) {
+        ArrayList<Docente> tuttiIDocenti = Elenchi.docenti;
+        ArrayList<Docente> docentiLiberi;
+        Ora oraCercata = new Ora(1, 1);
+        
+        docentiLiberi = FiltroLibero.docentiLiberi(tuttiIDocenti, oraCercata);
+        for(int i=0; i<docentiLiberi.size(); i++) {
+            System.out.println(docentiLiberi.get(i));
+        }
+    }
+
+}
