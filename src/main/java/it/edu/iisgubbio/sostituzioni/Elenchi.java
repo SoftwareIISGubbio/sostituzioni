5d76893493f9ff001503e7f233f67ffe44452771
Michele Vantaggi <michelevantaggi02@gmail.com>
2019-12-09 09:31:42 +0100

    modificato file elenchi

--- src/main/java/it/edu/iisgubbio/sostituzioni/Elenchi.java
+++ src/main/java/it/edu/iisgubbio/sostituzioni/Elenchi.java
@@ -3,7 +3,6 @@ package it.edu.iisgubbio.sostituzioni;
 import java.util.ArrayList;
 
 import it.edu.iisgubbio.sostituzioni.oggetti.Docente;
-import it.edu.iisgubbio.sostituzioni.oggetti.OraLezione;
 
 public class Elenchi {
     public static ArrayList<Docente> docenti = new ArrayList<>();
