cfce751e812946855cb15f1a3ee8ee19078e0ad7
Jennifer Ermirandi <jenny-91@outlook.it>
2019-12-19 11:37:57 +0100

    Modificato finestra principale per elenco docenenti, miglioramenti grafici, aggiunta combobox per lista docenti, creato un menù in alto con e informazioni del software!

--- src/main/java/it/edu/iisgubbio/sostituzioni/FinestraPrincipale.java
+++ src/main/java/it/edu/iisgubbio/sostituzioni/FinestraPrincipale.java
@@ -1,7 +1,6 @@
 package it.edu.iisgubbio.sostituzioni;
 
 import java.io.IOException;
-
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -19,6 +18,8 @@ public class FinestraPrincipale extends Application {
 	DatePicker data;
 	@FXML
 	ComboBox<String> cmbOra;
+	@FXML
+	ComboBox<String> nomeProf;
 	@FXML
 	ComboBox<String> cmbClasse;
 	@FXML
@@ -45,22 +46,33 @@ public class FinestraPrincipale extends Application {
 
 	@FXML
 	void initialize() {
+		// Ciclo per scorrere le classi e li inserisce alla combobox
 		String[] classi = Elenchi.getNomiClassi();
 		for (int i = 1; i <= 8; i++) {
 			String n = "" + i;
 			cmbOra.getItems().add(n);
 		}
-		for(int j = 0; j < classi.length; j++) {
+		for (int j = 0; j < classi.length; j++) {
 			cmbClasse.getItems().add(classi[j]);
 		}
-		
+		// Ciclo per scorrerel l'elenco dei professori e li inserisce alla combobox
+		for (int i = 1; i <= 8; i++) {
+			String n = "" + i;
+			cmbOra.getItems().add(n);
+		}
+
+		for (int j = 0; j < Elenchi.docenti.size(); j++) {
+			nomeProf.getItems().add(Elenchi.docenti.get(j).nome);
+		}
+
 	}
 
 	@FXML
 	private void gestioneClickPulsante(ActionEvent e) {
+		System.out.println(nomeProf.getValue());
 		System.out.println(data.getValue());
 		System.out.println(cmbOra.getValue());
-		System.out.println(cmbClasse.getValue());
+		System.out.println(cmbClasse.getValue()); 
 
 	}
 }
