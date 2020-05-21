76b086e99d6bb890233ea7747d7c16f3253cbcbb
Bianca <Bianca@DESKTOP-TIT14PB.station>
2020-05-21 17:23:32 +0200

    Ho inserito i commenti nella classe FiltroCoPresenza

--- src/main/java/it/edu/iisgubbio/sostituzioni/filtri/FiltroCoPresenza.java
+++ src/main/java/it/edu/iisgubbio/sostituzioni/filtri/FiltroCoPresenza.java
@@ -7,20 +7,24 @@ import it.edu.iisgubbio.sostituzioni.oggetti.Ora;
 import it.edu.iisgubbio.sostituzioni.oggetti.OraLezione;
 
 public class FiltroCoPresenza {
-	
+
 	/**
-	 * 
 	 * @param tutti rappresenta un elenco di docenti
 	 * @param oraDaSostituire ora in cui viene richiesta la sostituzione
-	 * @return l'elenco di professori che sono in compresenza nella classe e nell'ora richiesta
+	 * @return l'elenco dei professori che sono in copresenza nella classe e nell'ora richiesta
+	 * @author Bianca
 	 */
 
 	public static ArrayList<Docente> docentiCoPresenza(ArrayList<Docente> tutti, OraLezione oraDaSostituire) {
+		// lista dei docenti in copresenza
 		ArrayList<Docente> risposta = new ArrayList<>();
+		// ora del giorno in cui viene richiesta la sostituzione
 		Ora ora = new Ora(oraDaSostituire.giorno, oraDaSostituire.orario);
+		// la classe che ha bisogno della sostituzione
 		String classe = oraDaSostituire.classe;
-
+		// ricerca del docente in copresenza tra tutti i docenti
 		for (Docente d : tutti) {
+			// se il docente è presente nell'ora inserita nella classe che necessita la sostituzione significa che è in copresenza 
 			if (d.lavoraNellaClasseInOra(ora, classe)) {
 
 				risposta.add(d);
