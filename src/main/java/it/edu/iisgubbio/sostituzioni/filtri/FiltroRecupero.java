4169fb7bd376a6fadee478d4c342cfefff7771e1
Bianca <Bianca@DESKTOP-TIT14PB.station>
2020-05-21 17:58:48 +0200

    Ho aggiunto i commenti sulle classi FiltroRecupero e RimozioneDocente

--- src/main/java/it/edu/iisgubbio/sostituzioni/filtri/FiltroRecupero.java
+++ src/main/java/it/edu/iisgubbio/sostituzioni/filtri/FiltroRecupero.java
@@ -6,12 +6,19 @@ import it.edu.iisgubbio.sostituzioni.oggetti.Docente;
 import it.edu.iisgubbio.sostituzioni.oggetti.Ora;
 
 public class FiltroRecupero {
-
+	/**
+	 * @param tutti rappresenta un elenco di docenti
+	 * @param oraDaRecuperare ora in cui il docente è disponibile a fare recupero
+	 * @return l'elenco dei professori che mettono a disposizione un'ora di recupero in un determinato giorno
+	 * @author Bianca
+	 */
 	public static ArrayList<Docente> docentiRecupero(ArrayList<Docente> tutti, Ora oraDaRecuperare) {
 		ArrayList<Docente> risposta = new ArrayList<>();
-		
+		// cerca i docenti nella lista di tutti i docenti
 		for (Docente d : tutti) {
+			// esclude i prof che non hanno ore di recupero
 			if(d.oraARecupero!=null) {
+				// se l'ora di recupero di un docente corrisponde con l'ora in un determinato giorno in cui c'è bisogno di recupero, allora aggiunge il docente alla lista
 				if (d.oraARecupero.giorno == oraDaRecuperare.giorno && d.oraARecupero.orario == oraDaRecuperare.orario ) {
 
 					risposta.add(d);
--- src/main/java/it/edu/iisgubbio/sostituzioni/filtri/RimozioneDocente.java
+++ src/main/java/it/edu/iisgubbio/sostituzioni/filtri/RimozioneDocente.java
@@ -5,11 +5,18 @@ import java.util.ArrayList;
 import it.edu.iisgubbio.sostituzioni.oggetti.Docente;
 
 public class RimozioneDocente {
-
+	/**
+	 * Questo filtro serve per rimuovere dall'elenco di tutti i docenti, il docente che è assente
+	 * @param tutti rappresenta un elenco di docenti
+	 * @param docenteDaRimuovere il docente mancante per cui c'è bisogno della sostituzione
+	 * @return elenco dei docenti senza il docente assente
+	 * @author Bianca
+	 */
 	public static ArrayList<Docente> docentiRimozione(ArrayList<Docente> tutti, String docenteDaRimuovere) {
 		ArrayList<Docente> risposta = new ArrayList<>();
-
+		// scorre l'elenco dei docenti 
 		for (Docente d : tutti) {
+			// se il nome del docente è diverso dal docente assente, viene aggiunto alla lista 
 			if (!d.nome.equals(docenteDaRimuovere)) {
 				risposta.add(d);
 			}
