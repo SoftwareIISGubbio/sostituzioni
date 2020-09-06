e8fcb655ad16af57993326f9f995ccd950e99fee
Edoardo Panfili <edoardopa@gmail.com>
2020-09-06 17:30:27 +0200

    lo script per la creazione del pacchetto mette anche il numero di versione

--- creazionePacchetto.sh
+++ creazionePacchetto.sh
@@ -1,26 +1,30 @@
 #!/bin/bash
-# ATTENZIONE: questo script fa affidamento sulla variabile JAVA_HOME
-# sulla struttura delle cartelle del progetto e su quelle create da maven
-
-# la costruzione del pacchetto è piuttosto "manuale", 
-# sul sito di javafx ci sono anche indicazioni per maven.
+# ATTENZIONE: questo script fa affidamento su una serie di cose più o meno standard
+# - la variabile JAVA_HOME deve essere impostata
+# - maven deve essere installato e sul path
+# - il progetto usa maven 
+# - mvn package mette tutti i jar in target/jars
+# - le icone sono nella cartella icone
 
 if [ "$#" -ne 1 ]; then
     echo "uso del programma:"
     echo "creazionePacchetto.sh <path_cartella_principale_javafx_sdk>"
 fi
 
+# ilprimo parametro è la poszione dell'SDK di JavaFX
 CARTELLA_FX_SDK=$1
 # comando per jpackage
 JPACKAGE=$JAVA_HOME/bin/jpackage
-# dove stanno i jar (cartella impostata nel file pom.xml}
+# dove stanno i jar (cartella impostata nel file pom.xml)
 CARTELLA_JARS=target/jars
-# nome del jar principale (contiene anche il numero di versione)
-JAR_PRINCIPALE=$(cd $CARTELLA_JARS; ls sostituzioni*.jar)
 # cartella di lavoro
 CARTELLA_LAVORO=target/lavoro
 # dove mettere il file compilato
 DESTINAZIONE=target
+# versione (recuperata dal file pom)
+VERSIONE=$(grep -m 1 version pom.xml | sed 's/[^0-9\\.]//g')
+# nome del jar principale (contiene anche il numero di versione)
+JAR_PRINCIPALE="sostituzioni-$VERSIONE.jar"
 # nome icona, dipende dal sistema operativo
 if [[ "$OSTYPE" == "darwin"*  ]]; then
     # icona per macOS
@@ -45,6 +49,7 @@ echo ""
 echo "CARTELLA_JARS  : $CARTELLA_JARS"
 echo "JAR_PRINCIPALE : $JAR_PRINCIPALE"
 echo "ICONA          : $ICONA"
+echo "VERSIONE       : $VERSIONE"
 echo ""
 echo "CARTELLA_LAVORO: $CARTELLA_LAVORO"
 echo "DESTINAZIONE   : $DESTINAZIONE"
@@ -67,6 +72,9 @@ else
     cp $CARTELLA_FX_SDK/lib/* $CARTELLA_LAVORO
 fi
 
-$JPACKAGE --dest $DESTINAZIONE --type $TIPO_PACCHETTO -n sostituzioni --icon $ICONA --input $CARTELLA_LAVORO \
-    --add-modules javafx.controls,javafx.media,javafx.fxml,jdk.charsets --module-path $CARTELLA_LAVORO \
+$JPACKAGE --name sostituzioni --app-version $VERSIONE --icon $ICONA --type $TIPO_PACCHETTO \
+    --input $CARTELLA_LAVORO --dest $DESTINAZIONE --module-path $CARTELLA_LAVORO \
+    --add-modules javafx.controls,javafx.media,javafx.fxml,jdk.charsets \
     --main-class it.edu.iisgubbio.sostituzioni.FinestraPrincipale --main-jar $JAR_PRINCIPALE
+    
+# su windows non sarebbe male creare lo zip
