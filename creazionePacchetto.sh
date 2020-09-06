10879ff2ea71f58f43ecfd3f99e735d1bb1d4615
Edoardo Panfili <edoardopa@gmail.com>
2020-09-06 16:48:45 +0200

    aggiunto script per creare pacchetti

---
+++ creazionePacchetto.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+# ATTENZIONE: questo script fa affidamento sulla variabile JAVA_HOME
+# sulla struttura delle cartelle del progetto e su quelle create da maven
+
+# la costruzione del pacchetto è piuttosto "manuale", 
+# sul sito di javafx ci sono anche indicazioni per maven.
+
+if [ "$#" -ne 1 ]; then
+    echo "uso del programma:"
+    echo "creazionePacchetto.sh <path_cartella_principale_javafx_sdk>"
+fi
+
+CARTELLA_FX_SDK=$1
+# comando per jpackage
+JPACKAGE=$JAVA_HOME/bin/jpackage
+# dove stanno i jar (cartella impostata nel file pom.xml}
+CARTELLA_JARS=target/jars
+# nome del jar principale (contiene anche il numero di versione)
+JAR_PRINCIPALE=$(cd $CARTELLA_JARS; ls sostituzioni*.jar)
+# cartella di lavoro
+CARTELLA_LAVORO=target/lavoro
+# dove mettere il file compilato
+DESTINAZIONE=target
+# nome icona, dipende dal sistema operativo
+if [[ "$OSTYPE" == "darwin"*  ]]; then
+    # icona per macOS
+    ICONA=icona/icona.icns
+    TIPO_PACCHETTO="dmg"
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+    # icona le Linux (l'unico normale visto il tipo del file!)
+    ICONA=icona/icona.png
+    TIPO_PACCHETTO="app-image"
+else
+    # icona per Windows
+    ICONA=icona/icona.ico
+    TIPO_PACCHETTO="app-image"
+fi
+
+echo "----- ambiente di lavoro -------------------------------------"
+echo "JAVA_HOME      : $JAVA_HOME"
+echo "JPACKAGE       : $JPACKAGE"
+echo "CARTELLA_FX_SDK: $CARTELLA_FX_SDK"
+echo "MAVEN          : $(which mvn)"
+echo ""
+echo "CARTELLA_JARS  : $CARTELLA_JARS"
+echo "JAR_PRINCIPALE : $JAR_PRINCIPALE"
+echo "ICONA          : $ICONA"
+echo ""
+echo "CARTELLA_LAVORO: $CARTELLA_LAVORO"
+echo "DESTINAZIONE   : $DESTINAZIONE"
+echo "TIPO_PACCHETTO : $TIPO_PACCHETTO"
+echo "--------------------------------------------------------------"
+
+mvn clean
+mvn package -DskipTests
+
+mkdir $CARTELLA_LAVORO
+cp $CARTELLA_JARS/* $CARTELLA_LAVORO
+rm $CARTELLA_LAVORO/javafx*
+if [[ "$OSTYPE" == "darwin"*  ]]; then
+    cp $CARTELLA_FX_SDK/lib/* $CARTELLA_LAVORO
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+    echo "mancano impostazioni per ambiente linux in questo script"
+    exit
+else
+    cp $CARTELLA_FX_SDK/bin/* $CARTELLA_LAVORO
+    cp $CARTELLA_FX_SDK/lib/* $CARTELLA_LAVORO
+fi
+
+$JPACKAGE --dest $DESTINAZIONE --type $TIPO_PACCHETTO -n sostituzioni --icon $ICONA --input $CARTELLA_LAVORO \
+    --add-modules javafx.controls,javafx.media,javafx.fxml,jdk.charsets --module-path $CARTELLA_LAVORO \
+    --main-class it.edu.iisgubbio.sostituzioni.FinestraPrincipale --main-jar $JAR_PRINCIPALE
