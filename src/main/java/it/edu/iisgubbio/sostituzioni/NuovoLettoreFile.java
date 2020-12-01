8398a0399e3ef4caf8955ae195c649d54d29bf41
Riginel <rigiungureanu@gmail.com>
2020-12-01 11:49:07 +0100

    creato prototipo lettore file nuovo

---
+++ src/main/java/it/edu/iisgubbio/sostituzioni/NuovoLettoreFile.java
@@ -0,0 +1,45 @@
+package it.edu.iisgubbio.sostituzioni;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import it.edu.iisgubbio.sostituzioni.oggetti.Docente;
+
+public class NuovoLettoreFile {
+	
+	private final static byte RIGA_ORE = 4;
+	private static final String FOGLIO_DOCENTI = "Insieme_totale";
+	private static final String FOGLIO_INFORMAZIONI = "informazioni";
+	
+	private final static byte PRIMO_INSEGNANTE = 5;
+	private final static byte  COLONNA_INSEGNANTE = 0;
+	public static ArrayList<Docente> leggiExcel(){
+		ArrayList<Docente> lista = new ArrayList<Docente>();
+		try {
+			Workbook libro = new XSSFWorkbook(new FileInputStream("C:\\Users\\rigiu\\git\\sostituzioni\\datiDiProva\\fileDatiDocenti.xlsx"));
+			Sheet foglio = libro.getSheet(FOGLIO_DOCENTI);
+			int i = PRIMO_INSEGNANTE;
+			String contenuto;
+			while((contenuto = foglio.getRow(i).getCell(COLONNA_INSEGNANTE).getStringCellValue()).length()!=0) {
+				System.out.println(i + " " + contenuto);
+				lista.add(new Docente(contenuto));
+				i+=2;
+				
+			}
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return(lista);
+	}
+	public static void main(String[] args) {
+		leggiExcel();
+	}
+}
