e3b0a5293bb2adb28cd2d0d1fc20b7a8f43cf56b
BuildTools <unconfigured@null.spigotmc.org>
2020-12-01 11:11:09 +0100

    Colorato casella dei professori in base al tipo di sostituzione

--- src/main/java/it/edu/iisgubbio/sostituzioni/gui/CasellaSostituzione.java
+++ src/main/java/it/edu/iisgubbio/sostituzioni/gui/CasellaSostituzione.java
@@ -19,13 +19,55 @@ public class CasellaSostituzione extends ListCell<Sostituzione>{
         if (item == null || empty) {
         
         } else {
-            if(item.getMotivazione().equals("a disposizione Cassata") || 
+            /*if(item.getMotivazione().equals("a disposizione Cassata") || 
                     item.getMotivazione().equals("libero della classe") ||
                     item.getMotivazione().equals("libero altra classe") ) {
                 this.setTextFill(Color.GRAY);
             }else {
                 this.setTextFill(Color.BLACK);
-            }
+            }*/
+        	
+        	this.setTextFill(Color.rgb(255, 255, 255));
+        	
+        	switch(item.getMotivazione()) {
+        	case("copresenza"):
+        		this.setStyle("-fx-background-color: #CC0000");
+        		break;
+        	case("potenziamento stesse discipline"):
+        		this.setStyle("-fx-background-color: #FF0000");
+        		break;
+        	case("potenziamento altre discipline"):
+        		this.setStyle("-fx-background-color: #FF8000");
+        		break;
+        	case("recupero stessa classe"):
+        		this.setStyle("-fx-background-color: #FFFF00");
+        		break;
+        	case("recupero altra classe, stesso gruppo"):
+        		this.setStyle("-fx-background-color: #80FF00");
+        		break;
+        	case("recupero altra classe, altro gruppo"):
+        		this.setStyle("-fx-background-color: #33FF33");
+        		break;
+        	case("a pagamento stessa classe"):
+        		this.setStyle("-fx-background-color: #33FF99");
+        		break;
+        	case("a pagamento altra classe ma stesso gruppo"):
+        		this.setStyle("-fx-background-color: #33FFFF");
+        		break;
+        	case("a pagamento altra classe e altro gruppo"):
+        		this.setStyle("-fx-background-color: #66B2FF");
+        		break;
+        	case("a disposizione Cassata"):
+        		this.setStyle("-fx-background-color: #6666FF");
+        		break;
+        	case("libero della classe"):
+        		this.setStyle("-fx-background-color: #B266FF");
+        		break;
+        	case("libero altra classe"):
+        		this.setStyle("-fx-background-color: #FF99FF");
+        		break;
+        	}
+        	
             Docente bersaglio = Ambiente.cercaDocentePerNome(item.getNomeSostituto());
             
             // this.setBackground(new Background(new BackgroundFill( Color.RED, null, null)));
