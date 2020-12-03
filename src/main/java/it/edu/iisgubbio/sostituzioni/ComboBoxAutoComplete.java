52bd33ee528f741a2b3b80acc2f50c4c0d3afb1d
Edoardo Panfili <edoardopa@gmail.com>
2020-12-03 19:30:09 +0100

    il tooltip si posiziona sopra l'elemento

--- src/main/java/it/edu/iisgubbio/sostituzioni/ComboBoxAutoComplete.java
+++ src/main/java/it/edu/iisgubbio/sostituzioni/ComboBoxAutoComplete.java
@@ -9,6 +9,7 @@ import javafx.scene.control.ComboBox;
 import javafx.scene.control.Tooltip;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
+import javafx.stage.PopupWindow.AnchorLocation;
 import javafx.stage.Window;
 
 // http://fxapps.blogspot.com/2016/03/simplest-javafx-combobox-autocomplete.html
@@ -21,7 +22,8 @@ public class ComboBoxAutoComplete<T> {
     public ComboBoxAutoComplete(ComboBox<T> cmb) {
         this.cmb = cmb;
         originalItems = FXCollections.observableArrayList(cmb.getItems());
-        cmb.setTooltip(new Tooltip());
+        Tooltip tt = new Tooltip();
+        cmb.setTooltip(tt);
         cmb.setOnKeyPressed(this::handleOnKeyPressed);
         cmb.setOnHidden(this::handleOnHiding);
     }
@@ -49,9 +51,14 @@ public class ComboBoxAutoComplete<T> {
             itens.filter(el -> el.toString().toLowerCase().contains(txtUsr)).forEach(filteredList::add);
             cmb.getTooltip().setText(txtUsr);
             Window stage = cmb.getScene().getWindow();
-            double posX = stage.getX() + cmb.getBoundsInParent().getMinX();
-            double posY = stage.getY() + cmb.getBoundsInParent().getMinY();
-            cmb.getTooltip().show(stage, posX, posY);
+            // queste tre righe sotto erano del widget originale
+            // double posX = stage.getX() + cmb.getBoundsInParent().getMinX();
+            // double posY = stage.getY() + cmb.getBoundsInParent().getMinY();
+            // cmb.getTooltip().show(stage, posX, posY);
+            // le tre sotto le ho messe io (edoardo) e pare che il posizionamento vada meglio
+            cmb.getTooltip().setAnchorLocation(AnchorLocation.CONTENT_TOP_LEFT);
+            cmb.getTooltip().setAutoFix(true);
+            cmb.getTooltip().show(stage);
             cmb.show();
         }
         cmb.getItems().setAll(filteredList);
