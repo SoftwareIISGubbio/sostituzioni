376a64af2aa49b313b456a1baf70a88b96d744ea
Riginel <rigiungureanu@gmail.com>
2020-12-11 09:58:06 +0100

    aggiunti commenti e completata lettura file TRANNE SOSTEGNO

--- src/main/java/it/edu/iisgubbio/sostituzioni/NuovoLettoreFile.java
+++ src/main/java/it/edu/iisgubbio/sostituzioni/NuovoLettoreFile.java
@@ -1,5 +1,6 @@
 package it.edu.iisgubbio.sostituzioni;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -9,8 +10,14 @@ import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 import it.edu.iisgubbio.sostituzioni.oggetti.Docente;
+import it.edu.iisgubbio.sostituzioni.oggetti.Ora;
 import it.edu.iisgubbio.sostituzioni.oggetti.OraLezione;
-
+/**
+ * Legge il documento excel 
+ * e inserisce le informazioni in un arraylist docenti
+ * @author Riginel Andrei Ungureanu
+ *
+ */
 public class NuovoLettoreFile {
 
 	private static final String MARCATORE_ORA_RECUPERO = "R";
@@ -26,42 +33,99 @@ public class NuovoLettoreFile {
 
 	private final static byte PRIMO_INSEGNANTE = 5;
 	private final static byte COLONNA_INSEGNANTE = 0;
-
 	
-	public static ArrayList<Docente> leggiExcel() {
+	/**
+	 * legge il file excel e restituisce la lista degli insegnanti
+	 * 
+	 * @param percorso del file
+	 *   
+	 * @return un arraylist con dentro tutti i docenti
+	 */
+	public static ArrayList<Docente> leggiExcel(File percorso) {
 		ArrayList<Docente> lista = new ArrayList<Docente>();
 		try {
-			Workbook libro = new XSSFWorkbook(
-					new FileInputStream("C:\\Users\\rigiu\\git\\sostituzioni\\datiDiProva\\fileDatiDocenti2020.xlsx"));
+			Workbook libro = new XSSFWorkbook(new FileInputStream(percorso));
 			Sheet foglio = libro.getSheet(FOGLIO_DOCENTI);
-			//indice degli insegnanti (righe)
+			// indice degli insegnanti (righe)
 			int i = PRIMO_INSEGNANTE;
 			String contenuto;
-			
-			//scorro fino a quando arriva alla fine degli insegnanti
+
+			// scorro fino a quando arriva alla fine degli insegnanti
 			while ((contenuto = foglio.getRow(i).getCell(COLONNA_INSEGNANTE).getStringCellValue()).length() != 0) {
 				// System.out.println(i + " " + contenuto);
 				Docente d = new Docente(contenuto);
-				//scorre le ore dell'orario(colonne)
+				// scorre le ore dell'orario(colonne)
 				for (int j = 1; j < COLONNA_FINALE_ORARIO; j++) {
-					//controlla se a quell'ora il professore lavora
+					// controlla se a quell'ora il professore lavora
 					if (foglio.getRow(i).getCell(j).getStringCellValue().length() != 0) {
-						
-						int giorno =  ((j-(j%9)+ 1) /9)+1;
-						int orario = j%9;
-						String aula = foglio.getRow(i+1).getCell(j).getStringCellValue();
+						//calcolo il numero del giorno(da 1 a 5)
+						int giorno = ((j - (j % 9) + 1) / 9) + 1;
+						//calcolo l'ora di lezione(da 1 a 8 )
+						int orario = j%9 > 6 ? j%9 : j%9+1 ;
+						//System.out.println(orario);
+						String aula = foglio.getRow(i + 1).getCell(j).getStringCellValue();
 						String classe = foglio.getRow(i).getCell(j).getStringCellValue();
-						//temporaneo
-						boolean compresenza = false;
-						d.oreLezione.add(new OraLezione(giorno,orario,aula,classe,compresenza));
-						//System.out.println(giorno +" " + (((j-(j%9)+ 1) /9)+1));
+						boolean compresenza = compresenza(foglio,i,j,aula);
+						
+						switch (aula) {
+							case MARCATORE_ORA_RECUPERO:
+								d.oraARecupero = new Ora(giorno, orario);
+								break;
+								
+							case MARCATORE_ORA_POTENZIAMENTO:
+								d.orePotenziamento.add(new Ora(giorno, orario));
+								break;
+								
+							case MARCATORE_ORA_PAGAMENTO:
+								d.oreAPagamento.add(new Ora(giorno, orario));
+								break;
+								
+							case MARCATORE_ORA_DISPOSIZIONE_GATTAPONE:
+								d.oraADisposizioneGattapone = new Ora(giorno, orario);
+								break;
+								
+							case MARCATORE_ORA_DISPOSIZIONE_CASSATA:
+								d.oreADisposizioneCassata.add(new Ora(giorno, orario));
+								break;
+								
+							case "":
+								break;
+								
+							default:
+								d.oreLezione.add(new OraLezione(giorno, orario, aula, classe, compresenza));
+								System.out.println("Prof "+ contenuto + " giorno " + giorno+ " orario "+ orario + " Compresenza: "+compresenza(foglio,i,j,aula));
+								
+						}
+
+						// System.out.println(giorno +" " + (((j-(j%9)+ 1) /9)+1));
 					}
 				}
-				i += 2;
 				
+				
+				
+				i += 2;
 				lista.add(d);
 			}
-
+			
+			Sheet fGruppi = libro.getSheet(FOGLIO_INFORMAZIONI);
+			i = 0;
+			while ( fGruppi.getRow(i+1)!=null ) {
+				String gruppo =  fGruppi.getRow(i+1).getCell(1).getStringCellValue();
+				int oreARecupero =(int) fGruppi.getRow(i+1).getCell(2).getNumericCellValue();
+				lista.get(i).gruppo = gruppo;
+				lista.get(i).oreDaRecuperare = oreARecupero;
+				//System.out.println(contenuto);
+				
+				
+				i++;
+			}
+			//System.out.println("FINITO!");
+			for( i = 0; i< lista.size();i++) {
+				Docente d = lista.get(i);
+				System.out.println(d.nome+","+d.gruppo + "," + d.oreDaRecuperare);
+				
+				
+			}
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -69,8 +133,35 @@ public class NuovoLettoreFile {
 
 		return (lista);
 	}
+	
+	
+	/**
+	 * Controlla se un insegnante ha la compresenza
+	 * @param foglio da dove si prende la tabella 
+	 * @param indiceInsegnante riga dell'insegnante che si deve controllare
+	 * @param indiceOra colonna dell'insegnante che si deve controllare
+	 * @param aula aula dove avviene la possibile compresenza
+	 * @return true se c'è la compresenza, falso altriemnti
+	 */
+	public static boolean compresenza(Sheet foglio, int indiceInsegnante,int indiceOra, String aula) {
+		boolean  risposta = false;
+		int i = PRIMO_INSEGNANTE;
+		
+		while ((foglio.getRow(i).getCell(COLONNA_INSEGNANTE).getStringCellValue()).length() != 0) {
+			if(i != indiceInsegnante) {
+				if(foglio.getRow(i+1).getCell(indiceOra).getStringCellValue().equals(aula)) {
+					risposta = true;
+					break;
+				}
+			}
+			i+=2;
+		}
+		
+		return risposta;
+	}
 
 	public static void main(String[] args) {
-		leggiExcel();
+		leggiExcel(new File("C:\\Users\\rigiu\\git\\sostituzioni\\datiDiProva\\fileDatiDocenti2020.xlsx"));
+	
 	}
 }
