01110c63e7ae34d57b1df5980a3fd49b2961d4b5
SoftwareIISGubbio <software@iisgubbio.edu.it>
2020-12-11 15:36:01 +0100

    Aggiornato test per CSV

--- src/test/java/it/edu/iisgubbio/sostituzioni/TestCSV.java
+++ src/test/java/it/edu/iisgubbio/sostituzioni/TestCSV.java
@@ -8,32 +8,69 @@ public class TestCSV {
         String t;
         
         t = CSV.toCSV(v);
-        System.out.println("\n"+Arrays.toString(v) );
-        System.out.println( t );
+        System.out.print(Arrays.toString(v) + " → " + t +" : ");
         if("uno,due".equals(t) || "\"uno\",\"due\"".equals(t)) {
-            System.out.println("ok");
+            System.out.println("OK");
         }else {
-            System.out.println("errore");
+            System.out.println("ERRORE");
         }
         
         v[0] = "u,no";
         t = CSV.toCSV(v);
-        System.out.println("\n"+Arrays.toString(v) );
-        System.out.println( t );
+        System.out.print(Arrays.toString(v) + " → " + t +" : ");
         if("\"u,no\",due".equals(t) || "\"u,no\",\"due\"".equals(t)) {
-            System.out.println("ok");
+            System.out.println("OK");
         }else {
-            System.out.println("errore");
+            System.out.println("ERRORE");
         }
         
         v[0] = "u\"no";
         t = CSV.toCSV(v);
-        System.out.println("\n"+Arrays.toString(v) );
-        System.out.println( t );
+        System.out.print(Arrays.toString(v) + " → " + t +" : ");
         if("\"u\"\"no\",due".equals(t) || "\"u\"\"no\",\"due\"".equals(t)) {
-            System.out.println("ok");
+            System.out.println("OK");
         }else {
-            System.out.println("errore");
+            System.out.println("ERRORE");
         }
+        
+        
+        
+        t = "\"uno\",\"due\",\"tre\"";
+        v = CSV.fromCSV(t);
+        System.out.print(t + " → " + Arrays.toString(v) +" : ");
+        if( "uno-due-tre".equals( String.join("-", v) ) ) {
+            System.out.println("OK");
+        }else {
+            System.out.println("ERRORE");
+        }        
+        
+        t = "\"uno\",\"du\"\"e\",\"tre\"";
+        v = CSV.fromCSV(t);
+        System.out.print(t + " → " + Arrays.toString(v) +" : ");
+        if( "uno-du\"e-tre".equals( String.join("-", v) ) ) {
+            System.out.println("OK");
+        }else {
+            System.out.println("ERRORE");
+        }   
+        
+        
+        t = "uno,due,tre";
+        v = CSV.fromCSV(t);
+        System.out.print(t + " → " + Arrays.toString(v) +" : ");
+        if( "uno-due-tre".equals( String.join("-", v) ) ) {
+            System.out.println("OK");
+        }else {
+            System.out.println("ERRORE");
+        }   
+        
+        t = "\"uno\",due,\"tr,e\"";
+        v = CSV.fromCSV(t);
+        System.out.print(t + " → " + Arrays.toString(v) +" : ");
+        if( "uno-due-tr,e".equals( String.join("-", v) ) ) {
+            System.out.println("OK");
+        }else {
+            System.out.println("ERRORE");
+        }   
+
     }
 }
