50835bbba48bbfeadde8b0ad3f586e9eaa5400ee
SoftwareIISGubbio <software@iisgubbio.edu.it>
2020-12-14 21:21:55 +0100

    tavolozza colori per scelta
    dai committenti è arrivata una tavolozza colori per evidenziare i
    diversi tipi di scelta, quelle possibili sono state inserite, le altre
    segnalate come issue.

--- src/main/java/it/edu/iisgubbio/sostituzioni/gui/CasellaSostituzione.java
+++ src/main/java/it/edu/iisgubbio/sostituzioni/gui/CasellaSostituzione.java
@@ -23,43 +23,43 @@ public class CasellaSostituzione extends ListCell<Sostituzione>{
 
         	switch(item.getMotivazione()) {
         	case copresenza:
-        		this.setStyle("-fx-background-color: #CC0000");
+        		this.setStyle("-fx-background-color: #f7f7f7");
         		break;
         	case potenziamento_stesse_discipline:
-        		this.setStyle("-fx-background-color: #FF0000");
+        		this.setStyle("-fx-background-color: #ff7f57");
         		break;
         	case potenziamento_altre_discipline:
-        		this.setStyle("-fx-background-color: #FF8000");
+        		this.setStyle("-fx-background-color: #eeb597");
         		break;
         	case recupero_stessa_classe:
-        		this.setStyle("-fx-background-color: #FFFF00");
+        		this.setStyle("-fx-background-color: #6a87d8");
         		break;
         	case recupero_altra_classe_stesso_gruppo:
-        		this.setStyle("-fx-background-color: #80FF00");
+        		this.setStyle("-fx-background-color: #5390fe");
         		break;
         	case recupero_altra_classe_altro_gruppo:
-        		this.setStyle("-fx-background-color: #33FF33");
+        		this.setStyle("-fx-background-color: #adc8ff");
         		break;
         	case a_pagamento_stessa_classe:
-        		this.setStyle("-fx-background-color: #33FF99");
+        		this.setStyle("-fx-background-color: #3cb371");
         		break;
         	case a_pagamento_altra_classe_stesso_gruppo:
-        		this.setStyle("-fx-background-color: #33FFFF");
+        		this.setStyle("-fx-background-color: #48e44f");
         		break;
         	case a_pagamento_altra_classe_e_altro_gruppo:
-        		this.setStyle("-fx-background-color: #66B2FF");
+        		this.setStyle("-fx-background-color: #89ee8e");
         		break;
         	case a_disposizione_cassata:
-        		this.setStyle("-fx-background-color: #6666FF");
+        		this.setStyle("-fx-background-color: #ff8c00");
         		break;
         	case libero_della_classe:
-        		this.setStyle("-fx-background-color: #B266FF");
+        		this.setStyle("-fx-background-color: #9778ba");
         		break;
         	case libero_altra_classe:
-        		this.setStyle("-fx-background-color: #FF99FF");
+        		this.setStyle("-fx-background-color: #beabd4");
         		break;
         	default:
-        		this.setStyle("-fx-background-color: #727272");
+        		this.setStyle("-fx-background-color: white");
         	}
         	
             Docente bersaglio = Ambiente.cercaDocentePerNome(item.getNomeSostituto());
