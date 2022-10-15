8a8b301727eda57c56a6fdb28dedf63bed08f1b2
Edoardo <edoardo.panfili@iisgubbio.edu.it>
2022-10-15 09:44:35 +0200

    rimossi import mnon usati e inserito separatore del path generico

--- src/main/java/it/edu/iisgubbio/sostituzioni/FinestraEsportaGiornata.java
+++ src/main/java/it/edu/iisgubbio/sostituzioni/FinestraEsportaGiornata.java
@@ -1,11 +1,7 @@
 package it.edu.iisgubbio.sostituzioni;
 
-import java.io.BufferedWriter;
-
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
@@ -21,7 +17,6 @@ import it.edu.iisgubbio.sostituzioni.oggetti.Sostituzione;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.DatePicker;
-import javafx.scene.control.Label;
 
 /************************************************************************************************
 * crea la finestra per esportare in un foglio .xlsx tutte le supplenze della giornata
@@ -29,6 +24,7 @@ import javafx.scene.control.Label;
 public class FinestraEsportaGiornata {
     @FXML
     private void esportaGiornata(ActionEvent e) throws IOException {
+        System.out.println("bu");
     	//prende la data dal dataPicker e la trasforma in una variabile string
         LocalDate d = dataPicker.getValue();
         String data=d+"";
@@ -58,10 +54,8 @@ public class FinestraEsportaGiornata {
         //array list che contiene tutte le righe tranne le prime 2 (header1; header2)
         ArrayList<Row> righeProf = new ArrayList<Row>();
         
-        /********************************************************************************************
-    	 * Scrive l'intestazione del foglio, scrivendo le ORE nella prima riga (header1)
-    	 * e CLASSE SUPPLENTE MOTIVO nella seconda riga (header2)
-    	 *******************************************************************************************/
+    	// Scrive l'intestazione del foglio, scrivendo le ORE nella prima riga (header1)
+    	//  e CLASSE SUPPLENTE MOTIVO nella seconda riga (header2)
         for(int j=0; j<8; j++) {
 			Cell ora = header1.createCell(j*3+1);
 			ora.setCellValue(j+1+"° ORA");
@@ -76,11 +70,9 @@ public class FinestraEsportaGiornata {
 			motivo.setCellValue("motivo");
 		}
         
-        /********************************************************************************************
-    	 * aggiunge nell'array list "professoriDaSostituire" il nome del docente 
-    	 * da sostituire nel giorno selezionato;
-    	 * e aggiunge nell'array list "righeProf" le righe tante quante sono i prof.
-    	 *******************************************************************************************/
+        // aggiunge nell'array list "professoriDaSostituire" il nome del docente 
+    	// da sostituire nel giorno selezionato;
+    	// e aggiunge nell'array list "righeProf" le righe tante quante sono i prof.
         int count=2;
         for(int i=0; i<tutteSostituzioni.size(); i++) {
         	if(data.equals(tutteSostituzioni.get(i).getData())) {
@@ -126,13 +118,14 @@ public class FinestraEsportaGiornata {
         }
         
         //imposta come file location la cartella dove vengono salvati i biglietti
-        String fileLocation = Ambiente.getCartellaBiglietto()+"\\"+
+        String fileLocation = Ambiente.getCartellaBiglietto()+File.separator+
         		d.format(DateTimeFormatter.ofPattern("EEEE-yyyy-MMMM-dd"))+".xlsx";
         
         //scrive il file e lo chiude
         FileOutputStream outputStream = new FileOutputStream(fileLocation);
         workbook.write(outputStream);
         workbook.close();
+        System.out.println("done "+fileLocation);
     }
     @FXML
 	DatePicker dataPicker;
